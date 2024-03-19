c257ab3983d12f4e4f8b00438cc6855177c9f7e5
henryy0 <you@example.com>
2024-03-19 17:05:32 -0300

    reestruturação do codigo

--- Body/_rodape.php
+++
@@ -1,6 +0,0 @@
-<!-- rodape.php -->
-<footer class="footer mt-auto py-3 bg-dark">
-    <div class="container">
-        <span class="text-white"> CollaboraPro - <?php echo date("Y"); ?></span>
-    </div>
-</footer>
